0c0e0af6e12df8e4b7357b9296cea7887fd82832
Devon-McClendon <78507659+Devon-McClendon@users.noreply.github.com>
2023-02-11 15:17:22 -0500

    Basic Grappling Hook
    Basic grappling hook. Pulls directly to object. No swinging, no reach limit, no target restrictions

--- player.gd
+++ player.gd
@@ -1,14 +1,33 @@
 extends KinematicBody2D
 
+
 var velocity = Vector2()
+var chainVelocity := Vector2(0,0)
 var walkSpeed = 30
 var runSpeed = 45
 var playerSpeed = 0
 var onGround = false
+var hookPosition
+var currentRopeLength
+
 
 const GRAVITY = 10
 const JUMP_POWER = -250
 const FLOOR = Vector2(0,-1)
+const CHAIN_PULL = 50
+
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed:
+			# We clicked the mouse -> shoot()
+			hookPosition = event.position - get_viewport_rect().size * 0.5
+			currentRopeLength = global_position.distance_to(hookPosition)
+			$Grapple.shoot(hookPosition)
+		else:
+			# We released the mouse -> release()
+			$Grapple.release()
 
 
 func _physics_process(delta):
@@ -34,6 +53,15 @@ func _physics_process(delta):
 	velocity.y += GRAVITY
 	if is_on_floor():
 		onGround = true
-	#Move Player
-	velocity = move_and_slide(velocity, FLOOR)
+	# Hook physics
+	if $Grapple.hooked:
+		# `to_local($Chain.tip).normalized()` is the direction that the chain is pulling
+		chainVelocity = to_local($Grapple.tip).normalized() * CHAIN_PULL
+		velocity = move_and_slide(chainVelocity, FLOOR)
+	else:
+		# Not hooked -> no chain velocity
+		velocity = move_and_slide(velocity, FLOOR)
+		
+	
+	
 	
