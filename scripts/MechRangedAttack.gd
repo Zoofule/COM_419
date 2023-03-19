640255f7240083b3705c3ac4aabf3339a7721a8e
Devon-McClendon <78507659+Devon-McClendon@users.noreply.github.com>
2023-03-19 13:28:51 -0500

    Turret first demo
    Turret, only shots in one direction

--- scripts/MechRangedAttack.gd
+++ scripts/MechRangedAttack.gd
@@ -1,10 +1,10 @@
 extends Node2D
-const SPEED = 100
+export var speed = 100
 var velocity = Vector2.ZERO
 var direction = 1
 
 func _physics_process(delta):
-	velocity.x = SPEED * delta * direction
+	velocity.x = speed * delta * direction
 	translate(velocity)
 
 func _set_direction(dir):
